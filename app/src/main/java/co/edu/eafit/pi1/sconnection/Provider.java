2239b34209bf00f14f1cf92783853c405bba8262
tllano11 <tllano11@gmail.com>
2015-09-20 14:38:26 -0500

    Actividad "Provider" obtiene longitud y latitud. Posteriormente las evia al usuario por medio de un "socket".

--- app/src/main/java/co/edu/eafit/pi1/sconnection/Provider.java
+++ app/src/main/java/co/edu/eafit/pi1/sconnection/Provider.java
@@ -1,28 +1,48 @@
 package co.edu.eafit.pi1.sconnection;
 
-import android.net.wifi.WifiManager;
+import android.content.Context;
+import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Formatter;
 
-public class Provider extends AppCompatActivity {
+public class Provider extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location lastKnownLocation;
+    private String latitude, longitude;
+
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+
+    // Request code to use when launching the resolution activity
+    private final int REQUEST_RESOLVE_ERROR = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider);
+
+        if(!mResolvingError){
+            mGoogleApiClient.connect();
+        }
+
         ClientSender clientSender = new ClientSender("10.0.2.2",8880);
-        clientSender.setMessage("Hola-mundo---");
+        clientSender.setMessage(getCoordinates());
         clientSender.execute();
     }
 
@@ -48,6 +68,53 @@ public class Provider extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    /**************************** Google API connection *******************************************/
+
+    @Override
+    public void onConnected(Bundle connectionHint){
+        lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (lastKnownLocation != null) {
+            latitude = String.valueOf(lastKnownLocation.getLatitude());
+            longitude = String.valueOf(lastKnownLocation.getLongitude());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause){}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result){
+        if (mResolvingError) {return;}
+        else if (result.hasResolution()){
+            try{
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException sie){mGoogleApiClient.connect();}
+        } else {
+            mResolvingError = true;
+        }
+    }
+
+    /**************************** Google API connection *******************************************/
+
+    private synchronized void googleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private String getCoordinates(){
+        return longitude + '-' + latitude + "---";
+    }
+
     class ClientSender extends AsyncTask<Void, Void, Void>{
 
         private String serverAddress;
@@ -63,6 +130,10 @@ public class Provider extends AppCompatActivity {
 
         public void setMessage(String message){this.message = message;}
 
+        private String getLocation(){
+
+        };
+
         @Override
         public void onPreExecute(){super.onPreExecute();}
 
@@ -71,6 +142,7 @@ public class Provider extends AppCompatActivity {
             Socket s = null;
             DataOutputStream dos = null;
             PrintWriter out = null;
+
             try{
                 s = new Socket(serverAddress, serverPort);
                 dos = new DataOutputStream(s.getOutputStream());
