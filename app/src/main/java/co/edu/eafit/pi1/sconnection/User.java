b7687c9ece6f1516595002b662b19b8af97a7e32
ccorre20 <ccorre20@eafit.edu.co>
2015-09-20 10:30:56 -0500

    AsyncTask, socket

--- app/src/main/java/co/edu/eafit/pi1/sconnection/User.java
+++ app/src/main/java/co/edu/eafit/pi1/sconnection/User.java
@@ -1,8 +1,10 @@
 package co.edu.eafit.pi1.sconnection;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,13 +16,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class User extends AppCompatActivity {
 
     private Button b, b2;
     private ProgressBar progressBar;
     private TextView progressTxt;
-    private boolean listen;
+    private getDataTask task;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,84 +34,177 @@ public class User extends AppCompatActivity {
         b2 = (Button) findViewById(R.id.button_cancel);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressTxt = (TextView) findViewById(R.id.Progress_txt);
-        listen = false;
+        task = null;
     }
 
     public void connClickListener(View view){
-        this.b.setEnabled(false);
-        try{
-            ConnServer();
-        }catch (Exception e){
-            progressTxt.setText(R.string.Error);
-            progressBar.setProgress(0);
-            b.setEnabled(true);
-        }
+        task = new getDataTask(progressBar, progressTxt, b, b2);
+        task.execute();
     }
 
     public void cancelClickListener(View view){
-        listen = false;
-        this.b2.setEnabled(false);
-        this.b2.setVisibility(View.INVISIBLE);
-        progressTxt.setText(R.string.Error);
-        progressBar.setProgress(0);
-        b.setEnabled(true);
+        if(task != null) {
+            task.cancel(true);
+            task = null;
+        }
     }
 
-    private boolean isCanceled(){
-        return !this.listen;
-    }
+    private class getDataTask extends AsyncTask<Void, Integer, Void> {
 
-    private void ConnServer() throws Exception {
-        //TODO:Fix Sockets
-        this.b2.setVisibility(View.VISIBLE);
-        this.b2.setEnabled(true);
-        this.progressBar.setVisibility(View.VISIBLE);
-        this.progressTxt.setText(R.string.creating_server);
-        this.progressTxt.setVisibility(View.VISIBLE);
-        ServerSocket ss;
-        DataInputStream dis = null;
-        DataOutputStream dos = null;
-        Socket s = null;
+        private ProgressBar progressBar;
+        private TextView progressTxt;
+        private Button b, b2;
+        private int status;
+        private String result, publish;
 
-        try {
-            ss = new ServerSocket(8888);
-            this.progressBar.setProgress(20);
-            this.progressTxt.setText(R.string.waiting_for_connection);
-            listen = true;
-        } catch (Exception e) {
-            throw e;
+        public getDataTask(ProgressBar progressBar, TextView progressTxt, Button b, Button b2){
+            this.progressBar = progressBar;
+            this.progressTxt = progressTxt;
+            this.b = b;
+            this.b2 = b2;
+            status = 0;
         }
 
-        try {
-            s = ss.accept();
-            dis = new DataInputStream(s.getInputStream());
-            dos = new DataOutputStream(s.getOutputStream());
-        } catch (Exception e) {
-            //throw e;
-        } finally {
-            if (s != null) {
-                try {
-                    s.close();
-                } catch (Exception e) {
-                    //throw e;
+        protected void onPreExecute() {
+            b.setEnabled(false);
+            b2.setVisibility(View.VISIBLE);
+            b2.setEnabled(true);
+            progressBar.setVisibility(View.VISIBLE);
+            progressTxt.setVisibility(View.VISIBLE);
+            progressBar.setEnabled(true);
+            progressTxt.setEnabled(true);
+        }
+
+        protected Void doInBackground(Void... v) {
+            publishProgress(0);
+            ServerSocket ss = null;
+            DataInputStream dis = null;
+            DataOutputStream dos = null;
+            Socket s = null;
+            try {
+                ss = new ServerSocket(8888);
+                publishProgress(25);
+            } catch (Exception e) {
+                Log.v("test", e.getMessage());
+            }
+            try {
+                ss.setSoTimeout(5000);
+                s = ss.accept();
+                dis = new DataInputStream(s.getInputStream());
+                publishProgress(50);
+                result = "";
+                publish = "";
+                while((result = dis.readUTF()) != null){
+                    if(isCancelled()){
+                        result = "";
+                        publish = "";
+                        return null;
+                    }
+                    publish += result;
+                    if(result.contains("---")){
+                        break;
+                    }
+                }
+                publishProgress(75);
+            } catch (SocketTimeoutException e) {
+                Log.v("test", "timeout");
+                this.cancel(true);
+            }catch (Exception e){
+                Log.v("test", e.getMessage());
+                this.cancel(true);
+            } finally {
+                if(s != null){
+                    try{
+                        s.close();
+                    } catch (Exception e){
+                        //Log.v("test", e.getMessage());
+                    }
+                }
+                if(dis != null){
+                    try{
+                        dis.close();
+                    } catch(Exception e){
+                        //Log.v("test", e.getMessage());
+                    }
+                }
+                if(ss != null) {
+                    try {
+                        ss.close();
+                    } catch (Exception e) {
+                        //Log.v("test", e.getMessage());
+                    }
                 }
             }
-            if (dis != null) {
-                try {
-                    dis.close();
-                } catch (Exception e) {
-                    //throw e;
-                }
+            return null;
+        }
+
+        protected void onCancelled() {
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.INVISIBLE);
+            progressBar.setEnabled(false);
+            progressTxt.setText("Cancelled");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Log.v("test", e.getMessage());
             }
-            if (dos != null) {
-                try {
-                    dos.close();
-                } catch (Exception e) {
-                    //throw e;
+            progressTxt.setEnabled(false);
+            progressTxt.setVisibility(View.INVISIBLE);
+            b.setEnabled(true);
+            b2.setEnabled(false);
+            b2.setVisibility(View.INVISIBLE);
+        }
+
+        protected void onProgressUpdate(Integer... progress){
+            progressBar.setProgress(progress[0]);
+            switch (status){
+                case 0:
+                {
+                    progressTxt.setText(R.string.creating_server);
+                    status++;
+                    break;
+                }
+                case 1:
+                {
+                    progressTxt.setText(R.string.waiting_for_connection);
+                    status++;
+                    break;
+                }
+                case 2:
+                {
+                    progressTxt.setText(R.string.recieveing_data);
+                    status++;
+                    break;
+                }
+                case 3:
+                {
+                    progressTxt.setText(R.string.connection_complete);
+                    status++;
+                    break;
                 }
             }
         }
-        this.progressTxt.setText("out!");
+
+
+        protected void onPostExecute(Void v){
+            progressBar.setProgress(100);
+            progressBar.setVisibility(View.INVISIBLE);
+            progressBar.setEnabled(false);
+            progressTxt.setText("Done");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Log.v("test", e.getMessage());
+            }
+            progressTxt.setEnabled(false);
+            progressTxt.setVisibility(View.INVISIBLE);
+            b.setEnabled(true);
+            b2.setEnabled(false);
+            b2.setVisibility(View.INVISIBLE);
+
+            Log.v("test", publish);
+        }
+
     }
 
     @Override
