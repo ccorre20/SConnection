d0adf7e12c350d98a2a51c66a8714448a869b0ad
ivancardenas <icardena@eafit.edu.co>
2015-11-19 08:15:16 -0500

    Settings button deleted

--- app/src/main/java/co/edu/eafit/pi1/sconnection/UserProviderSearch.java
+++ app/src/main/java/co/edu/eafit/pi1/sconnection/UserProviderSearch.java
@@ -171,11 +171,6 @@ public class UserProviderSearch extends AppCompatActivity implements Receiver {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
