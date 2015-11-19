4dd084f6f4e429b1a344ba68b82b9d0197bc24c1
ivancardenas <icardena@eafit.edu.co>
2015-11-19 06:29:43 -0500

    Extras deleted

--- app/src/main/java/co/edu/eafit/pi1/sconnection/ProviderProfile.java
+++ app/src/main/java/co/edu/eafit/pi1/sconnection/ProviderProfile.java
@@ -50,6 +50,7 @@ public class ProviderProfile extends AppCompatActivity implements Receiver{
     }
 
     @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_provider_profile, menu);
